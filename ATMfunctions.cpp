fec1e49d43918c6452804f00d0aa640eedeebe6c
OmerZiv5 <151728463+OmerZiv5@users.noreply.github.com>
2024-03-23 09:33:40 +0200

    Update ATMfunctions.cpp

--- ATMfunctions.cpp
+++ ATMfunctions.cpp
@@ -1,5 +1,6 @@
 using namespace std;
 #include "bank.cpp"
+#include <algorithm>
 
 vector<Account>::iterator search_account(int acc_id){
     vector<Account>::iterator it = bank.accounts.begin();
@@ -11,24 +12,32 @@ vector<Account>::iterator search_account(int acc_id){
     return bank.accounts.end();
 }
 
+bool compare_account_num(const Account& a, const Account& b) { // I've made a change here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    return a.account_num < b.account_num;
+}
+
+
 // Opening a new account
 void create_account(int account_number,int password ,int initial_amount, int ATMid){
     pthread_mutex_lock(&bank.write_bank);
-
+    //cout << ATMid << " created an account" << endl;
     vector<Account>::iterator it = search_account(account_number);
     if(it == bank.accounts.end()){
         // Account does not exist in bank
         Account new_acc(initial_amount, account_number, password);
         bank.accounts.push_back(new_acc);
-        it = bank.accounts.end() - 1;
+        // I've made a change here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        sort(bank.accounts.begin(),bank.accounts.end(), compare_account_num);
         pthread_mutex_lock(&write_log);
-        cout << ATMid << ": New account id is " << new_acc.account_num << " with password " << new_acc.password << " and initial balance " << new_acc.balance << endl;
+        sleep(1);
+        log_file << ATMid << ": New account id is " << new_acc.account_num << " with password " << new_acc.password << " and initial balance " << new_acc.balance << endl;
         pthread_mutex_unlock(&write_log);
     }
     else{
         // Account already exists
         pthread_mutex_lock(&write_log);
-        cout << "Error " << ATMid << ": Your transaction failed - account with the same id exists" << endl;
+        sleep(1);
+        log_file << "Error " << ATMid << ": Your transaction failed - account with the same id exists" << endl;
         pthread_mutex_unlock(&write_log);
     }
 
@@ -48,8 +57,10 @@ void deposit_to_account(int account_number,int password ,int amount, int ATMid){
     vector<Account>::iterator it = search_account(account_number);
     // check if the account actually exists
     if(it == bank.accounts.end()){
+        //account does not exist
         pthread_mutex_lock(&write_log);
-        cout << "Error " << ATMid << ": Your transaction failed - account id " << account_number << " does not exist" << endl;
+        sleep(1);
+        log_file << "Error " << ATMid << ": Your transaction failed - account id " << account_number << " does not exist" << endl;
         pthread_mutex_unlock(&write_log);
     }
     else{
@@ -58,14 +69,16 @@ void deposit_to_account(int account_number,int password ,int amount, int ATMid){
         if(it->password != password){
             // Password is incorrect
             pthread_mutex_lock(&write_log);
-            cout << "Error " << ATMid << ": Your transaction failed - password for account id " << account_number << " is incorrect" << endl;
+            sleep(1);
+            log_file << "Error " << ATMid << ": Your transaction failed - password for account id " << account_number << " is incorrect" << endl;
             pthread_mutex_unlock(&write_log);
         }
         else{
             // Password is correct
             it->balance += amount;
             pthread_mutex_lock(&write_log);
-            cout << ATMid << ": Account " << it->account_num << " new balance is " << it->balance << " after amount " << amount << " $ was deposited" << endl;
+            sleep(1);
+            log_file << ATMid << ": Account " << it->account_num << " new balance is " << it->balance << " after amount " << amount << " $ was deposited" << endl;
             pthread_mutex_unlock(&write_log);
         }
         pthread_mutex_unlock(&it->write_account);
@@ -92,8 +105,10 @@ void withdraw_from_account(int account_number,int password ,int amount, int ATMi
     vector<Account>::iterator it = search_account(account_number);
     // check if the account actually exists
     if(it == bank.accounts.end()){
+        //account does not exist
         pthread_mutex_lock(&write_log);
-        cout << "Error " << ATMid << ": Your transaction failed - account id " << account_number << " does not exist" << endl;
+        sleep(1);
+        log_file << "Error " << ATMid << ": Your transaction failed - account id " << account_number << " does not exist" << endl;
         pthread_mutex_unlock(&write_log);
     }
     else{
@@ -102,20 +117,23 @@ void withdraw_from_account(int account_number,int password ,int amount, int ATMi
         if(it->password != password){
             // Password is incorrect
             pthread_mutex_lock(&write_log);
-            cout << "Error " << ATMid << ": Your transaction failed - password for account id " << account_number << " is incorrect" << endl;
+            sleep(1);
+            log_file << "Error " << ATMid << ": Your transaction failed - password for account id " << account_number << " is incorrect" << endl;
             pthread_mutex_unlock(&write_log);
         }
         else if(amount > it->balance){
             // Not enough money to withdraw
             pthread_mutex_lock(&write_log);
-            cout << "Error " << ATMid << ": Your transaction failed - account id " << account_number << " balance is lower than" << amount << endl;
+            sleep(1);
+            log_file << "Error " << ATMid << ": Your transaction failed - account id " << account_number << " balance is lower than" << amount << endl;
             pthread_mutex_unlock(&write_log);
         }
         else{
             // withdraw successful
             it->balance -= amount;
             pthread_mutex_lock(&write_log);
-            cout << ATMid << ": Account " << it->account_num << " new balance is " << it->balance << " after " << amount << " $ was withdrew" << endl;
+            sleep(1);
+            log_file << ATMid << ": Account " << it->account_num << " new balance is " << it->balance << " after " << amount << " $ was withdrew" << endl;
             pthread_mutex_unlock(&write_log);
         }
         pthread_mutex_unlock(&it->write_account);
@@ -141,8 +159,10 @@ void check_balance(int account_number, int password, int ATMid){
     vector<Account>::iterator it = search_account(account_number);
     // check if the account actually exists
     if(it == bank.accounts.end()){
+        //account does not exist
         pthread_mutex_lock(&write_log);
-        cout << "Error " << ATMid << ": Your transaction failed - account id " << account_number << " does not exist" << endl;
+        sleep(1);
+        log_file << "Error " << ATMid << ": Your transaction failed - account id " << account_number << " does not exist" << endl;
         pthread_mutex_unlock(&write_log);
     }
     else{
@@ -157,13 +177,15 @@ void check_balance(int account_number, int password, int ATMid){
         if(it->password != password){
             // Password is incorrect
             pthread_mutex_lock(&write_log);
-            cout << "Error " << ATMid << ": Your transaction failed - password for account id " << account_number << " is incorrect" << endl;
+            sleep(1);
+            log_file << "Error " << ATMid << ": Your transaction failed - password for account id " << account_number << " is incorrect" << endl;
             pthread_mutex_unlock(&write_log);
         }
         else{
             // password is correct
             pthread_mutex_lock(&write_log);
-            cout << ATMid << ": Account " << account_number << " balance is " << it->balance << endl;
+            sleep(1);
+            log_file << ATMid << ": Account " << account_number << " balance is " << it->balance << endl;
             pthread_mutex_unlock(&write_log);
         }
 
@@ -191,14 +213,16 @@ void close_account(int account_number, int password, int ATMid){
     if(it == bank.accounts.end()){
         // Account does not exist in bank
         pthread_mutex_lock(&write_log);
-        cout << "Error " << ATMid << ": Your transaction failed - account id " << account_number << " does not exist" << endl;
+        sleep(1);
+        log_file << "Error " << ATMid << ": Your transaction failed - account id " << account_number << " does not exist" << endl;
         pthread_mutex_unlock(&write_log);
     }
     else{
         // Account exists
         if(it->password != password){
             pthread_mutex_lock(&write_log);
-            cout << "Error " << ATMid << ": Your transaction failed - password for account id " << account_number << " is incorrect" << endl;
+            sleep(1);
+            log_file << "Error " << ATMid << ": Your transaction failed - password for account id " << account_number << " is incorrect" << endl;
             pthread_mutex_unlock(&write_log);
         }
         else{
@@ -206,7 +230,8 @@ void close_account(int account_number, int password, int ATMid){
             int balance = it->balance;
             bank.accounts.erase(it);
             pthread_mutex_lock(&write_log);
-            cout << ATMid << ": Account " << account_number << " is now closed. Balance was " << balance << endl;
+            sleep(1);
+            log_file << ATMid << ": Account " << account_number << " is now closed. Balance was " << balance << endl;
             pthread_mutex_unlock(&write_log);
         }
     }
@@ -227,13 +252,16 @@ void transfer(int source_account, int source_password, int target_account, int a
     vector<Account>::iterator target_it = search_account(target_account);
     // check if the account actually exists
     if(source_it == bank.accounts.end()){
+        //source account does not exist
         pthread_mutex_lock(&write_log);
-        cout << "Error " << ATMid << ": Your transaction failed - account id " << source_account << " does not exist" << endl;
+        sleep(1);
+        log_file << "Error " << ATMid << ": Your transaction failed - account id " << source_account << " does not exist" << endl;
         pthread_mutex_unlock(&write_log);
     }
-    else if(source_it == bank.accounts.end()){
+    else if(target_it == bank.accounts.end()){
         pthread_mutex_lock(&write_log);
-        cout << "Error " << ATMid << ": Your transaction failed - account id " << target_account << " does not exist" << endl;
+        sleep(1);
+        log_file << "Error " << ATMid << ": Your transaction failed - account id " << target_account << " does not exist" << endl;
         pthread_mutex_unlock(&write_log);
     }
     else{
@@ -242,20 +270,29 @@ void transfer(int source_account, int source_password, int target_account, int a
         pthread_mutex_lock(&target_it->write_account);
 
         if(source_it->password != source_password){
+            //source account password is incorrect
             pthread_mutex_lock(&write_log);
-            cout << "Error " << ATMid << ": Your transaction failed - password for account id " << source_account << " is incorrect" << endl;
+            sleep(1);
+            log_file << "Error " << ATMid << ": Your transaction failed - password for account id " << source_account << " is incorrect" << endl;
             pthread_mutex_unlock(&write_log);
         }
         else if(source_it->balance < amount){
+            //amount to tranfer is bigger than source balance
             pthread_mutex_lock(&write_log);
-            cout << "Error " << ATMid << ": Your transaction failed - account id " << source_account << " balance is lower than" << amount << endl;
+            sleep(1);
+            log_file << "Error " << ATMid << ": Your transaction failed - account id " << source_account << " balance is lower than" << amount << endl;
             pthread_mutex_unlock(&write_log);
         }
         else{
             // successful transaction
             source_it->balance -= amount;
             target_it->balance += amount;
+            pthread_mutex_lock(&write_log);
+            sleep(1);
+            log_file << ATMid << ": Transfer " << amount << " from account " << source_account << " to account " << target_account << " new account balance is " << source_it->balance << " new target account balance is " << target_it->balance << endl;
+            pthread_mutex_unlock(&write_log);
         }
+
 
         pthread_mutex_unlock(&target_it->write_account);
         pthread_mutex_unlock(&source_it->write_account);
