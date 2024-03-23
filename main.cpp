ab49fb0a5f15934a4b729dd5e9a68f879bd8ba80
OmerZiv5 <151728463+OmerZiv5@users.noreply.github.com>
2024-03-23 09:31:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,14 @@ using namespace std;
 #include <iostream>
 #include "ATMfunctions.cpp"
 #include <cstdio>
-#include <cstring>
+#include <string>
+#include <fstream>
+#include <sstream>
 
-int string_to_number(char* string){
-    int length = strlen(string);
+
+
+int string_to_number(string string){
+    int length = string.size();
     int result = 0;
     for(int i = length - 1; i >= 0; i--){
         int digit = string[i] - '0';
@@ -15,56 +19,49 @@ int string_to_number(char* string){
 }
 
 
-void command(char* line, int thread_num){
+void command(string line, int thread_num){
     // parse line into command fields
-    char delimiter[] = " ";
-    char *command_line;
+    stringstream ss(line);
+    char delimiter = ' ';
+    vector<string> command_line;
+    string token;
+    while(getline(ss, token, delimiter)){
+        command_line.push_back(token);
+    }
 
-    //command_line holds the operation we want to do (O,D,W,B,Q,T)
-    command_line = strtok(line, delimiter);
-    int operation = string_to_number(&command_line);
-    cout << "operation: " << operation << endl;
-    //command_line holds the account number
-    command_line = strtok(NULL, delimiter);
-    int account = string_to_number(&command_line);
-    cout << "account: " << account << endl;
-    //command_line holds the password for the account
-    command_line = strtok(NULL, delimiter);
-    int password = string_to_number(&command_line);
-    cout << "password: " << password << endl;
+    int account = string_to_number(command_line[1]);
+    int password = string_to_number(command_line[2]);
 
+    //cout << "Account: " << account << endl;
+    //cout<< "Password: " << password << endl;
     // executing ATM functions
-    if(operation == 'O'){
-        //command_line holds the initial amount to open an account
-        command_line = strtok(NULL, delimiter);
-        int initial_amount = string_to_number(&command_line);
+    if(command_line[0] == "O"){
+        int initial_amount = string_to_number(command_line[3]);
+        //cout << "Initial amount: " << initial_amount << endl;
         create_account(account, password, initial_amount, thread_num);
     }
-    else if(operation == 'D'){
-        //command_line holds the amount to deposit
-        command_line = strtok(NULL, delimiter);
-        int amount = string_to_number(&command_line);
+    else if(command_line[0] == "D"){
+        int amount = string_to_number(command_line[3]);
+        //cout << "Amount to deposit: " << amount << endl;
         deposit_to_account(account, password, amount, thread_num);
     }
-    else if(operation == 'W'){
-        //command_line holds the amount to withdraw
-        command_line = strtok(NULL, delimiter);
-        int amount = string_to_number(&command_line);
+    else if(command_line[0] == "W"){
+        int amount = string_to_number(command_line[3]);
+        //cout << "Amount to withdraw: " << amount << endl;
         withdraw_from_account(account, password, amount, thread_num);
     }
-    else if(operation == 'B'){
+    else if(command_line[0] == "B"){
+        //cout << "Checking balance..." << endl;
         check_balance(account, password, thread_num);
     }
-    else if(operation == 'Q'){
+    else if(command_line[0] == "Q"){
+        //cout << "Closing account..." << endl;
         close_account(account, password, thread_num);
     }
-    else if(operation == 'T'){
-        //command_line holds the target account number for the transfer
-        command_line = strtok(NULL, delimiter);
-        int target_account = string_to_number(&command_line);
-        //command_line holds the amount to transfer
-        command_line = strtok(NULL, delimiter);
-        int amount = string_to_number(&command_line);
+    else if(command_line[0] == "T"){
+        int target_account = string_to_number(command_line[3]);
+        int amount = string_to_number(command_line[4]);
+        //cout << "Transferring " << amount << "$ from Account: " << account << " to Target account: " << target_account << endl;
         transfer(account, password, target_account, amount, thread_num);
     }
 }
@@ -73,74 +70,156 @@ void command(char* line, int thread_num){
 void *ATM_function(void* ATM){
     thread_args* new_thread = (thread_args*)ATM;
     int thread_num = new_thread->thread_id;
-    const char* file = new_thread->filename;
-    FILE* command_file = fopen(file, "r");
-    if(!command_file){
-        perror("Bank error: fopen failed");
+    ifstream ATMfile(new_thread->filename);
+    if(!ATMfile.is_open()){
+        // Error opening file
+        cerr << "Bank error: illegal arguments" << endl;
         exit(-1);
     }
 
-    char line[256] = {};
-    while(fgets(line, 256, command_file) != NULL){
-        // replacing the last char in line with '\0'
-        if (line[strlen(line) - 1] == '\n') {
-            line[strlen(line) - 1] = '\0';
-        }
-        cout << "the line is: " << line << endl;
+    string line;
+    while(getline(ATMfile, line)){
+        // line contains a full line from the ATM file
+        //cout << "the line is: " << line << endl;
+        usleep(100000);
         command(line, thread_num);
-
-        // resetting the line buffer
-        memset(line, 0, sizeof(line));
     }
 
-    if(fclose(command_file)){
-        perror("Bank error: fclose failed");
+    ATMfile.close();
+    if(ATMfile.is_open()) {
+        cout << "ATM" << thread_num << " failed to close bitch!" << endl;
+        perror("Bank error: close failed");
         exit(-1);
     }
-
+    /*if(ATMfile.fail() || ATMfile.bad()) {
+        cout << "ATM" << thread_num << " failed to close bitch!" << endl;
+        perror("Bank error: close failed");
+        exit(-1);
+    }*/
     pthread_exit(nullptr);
 }
 
 
 
 int main(int argc, char**argv) {
+    //checking if arguments were passed
+    if(argc == 1){
+        cerr << "Bank error: illegal arguments" << endl;
+        exit(-1);
+    }
+
+    //opening log file
+    log_file.open("log.txt");
+    if(!log_file.is_open()){
+        //unable to open file
+        perror("Bank error: open failed");
+        exit(-1);
+    }
+
     // need to init mutex write_log
-    pthread_mutex_init(&write_log, NULL); //Sould we check the return value of the init????????????????????????????????
-    pthread_t* threads = new pthread_t[argc+1];
-    int* threadIDs = new int[argc-1];
+    int init_log = pthread_mutex_init(&write_log, NULL);
+    if(init_log){
+        //problem initializing log mutex
+        perror("Bank error: pthread_mutex_init failed\n");
+        exit(-1);
+    }
+
     //creating threads
+    pthread_t* threads = new pthread_t[argc+1];
+    /*int* threadIDs = new int[argc-1];*/
+
+    thread_args *ATM_args = new thread_args[argc-1];
     for(int i = 0; i < argc-1; i++){
-        threadIDs[i] = i;
-        thread_args ATM(argv[i+1], i);
-        int res = pthread_create(&threads[i], NULL, ATM_function, (void*)&ATM);
+        /*threadIDs[i] = i;*/
+        ATM_args[i].thread_id = i+1;
+        ATM_args[i].filename = argv[i+1];
+
+        int res = pthread_create(&threads[i], NULL, ATM_function, (void*)&ATM_args[i]);
         if(res != 0){
             //creating thread unsuccessful
-            // destroy write log mutex
-            pthread_mutex_destroy(&write_log);
+            // destroy write log mutex and deleting threads
+            int destroy_write_log = pthread_mutex_destroy(&write_log);
+            if(destroy_write_log){
+                perror("Bank error: pthread_mutex_destroy failed\n");
+            }
             delete[] threads;
-            delete[] threadIDs;
+            /*delete[] threadIDs;*/
+            delete[] ATM_args;
+            log_file.close();
+            if(!log_file){
+                //failed to close log file
+                cout << "ATM" << i+1 << " failed to create - log file unable to close biatch!" << endl;
+                perror("Bank error: close failed");
+            }
             exit(-1);
         }
     }
+
     int fee_res = pthread_create(&threads[argc-1], NULL, charge_fee, NULL);
     int status_res = pthread_create(&threads[argc], NULL, bank_status, NULL);
     if(fee_res || status_res){
         //creating thread unsuccessful
-        // destroy write log mutex
-        pthread_mutex_destroy(&write_log);
+        // destroy write log mutex and deleting threads
+        int destroy_write_log = pthread_mutex_destroy(&write_log);
+        if(destroy_write_log){
+            perror("Bank error: pthread_mutex_destroy failed\n");
+        }
         delete[] threads;
-        delete[] threadIDs;
+        /*delete[] threadIDs;*/
+        delete[] ATM_args;
+        log_file.close();
+        if(!log_file){
+            //failed to close log file
+            cout << "either charge fee thread or bank status thread failed to create - log file unable to close biatch!" << endl;
+            perror("Bank error: close failed");
+        }
         exit(-1);
     }
 
     // loop waiting for all threads to end
-    for(int i = 0; i < argc+1; i++){
-        pthread_join(threads[i], nullptr);
+    for(int i = 0; i < argc-1; i++){
+        int join = pthread_join(threads[i], nullptr);
+        if(join){
+            perror("Bank error: pthread_join failed\n");
+            int destroy_write_log = pthread_mutex_destroy(&write_log);
+            if(destroy_write_log){
+                perror("Bank error: pthread_mutex_destroy failed\n");
+            }
+            delete[] threads;
+            /*delete[] threadIDs;*/
+            delete[] ATM_args;
+            log_file.close();
+            if(!log_file){
+                //failed to close log file
+                cout << "either charge fee thread or bank status thread failed to create - log file unable to close biatch!" << endl;
+                perror("Bank error: close failed");
+            }
+            exit(-1);
+        }
     }
 
-    // destroy write log mutex
-    pthread_mutex_destroy(&write_log);
+    //all ATM threads are finished - need to stop bank status and comissioner
+    ATMs_finished = true;
+    int join_fee = pthread_join(threads[argc-1], nullptr);
+    int join_status = pthread_join(threads[argc], nullptr);
+    if(join_fee || join_status){
+        perror("Bank error: pthread_join failed\n");
+    }
+
+    // destroy write log mutex and threads
+    int destroy_write_log = pthread_mutex_destroy(&write_log);
+    if(destroy_write_log){
+        perror("Bank error: pthread_mutex_destroy failed\n");
+    }
     delete[] threads;
-    delete[] threadIDs;
+    delete[] ATM_args;
+    /*delete[] threadIDs;*/
+    log_file.close();
+    if(!log_file){
+        //failed to close log file
+        cout << "All good bruv but the log file was unable to close homie!" << endl;
+        perror("Bank error: close failed");
+        exit(-1);
+    }
     return 0;
 }
